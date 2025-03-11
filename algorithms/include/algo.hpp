70752ebb7bd07715cb04ac8bafedd905cf6e08de
Aiden McCallum <aidenmax@comcast.net>
2025-03-10 21:29:08 -0500

    Commiting more merged changes from combining

--- algorithms/include/algo.hpp
+++ algorithms/include/algo.hpp
@@ -105,4 +105,15 @@ long factorial(const int n)
     return output;
 }
 
+std::string fizz_buzz_checker(const int n) {
+    if (n % 15 == 0) {
+        return "fizzbuzz";
+    } else if (n % 5 == 0) {
+        return "buzz";
+    } else if (n % 3 == 0) {
+        return "fizz";
+    }
+    return "";
+}
+
 #endif // ALGO_H_
