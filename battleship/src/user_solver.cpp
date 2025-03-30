b0e0f40b0cc886943381dd1656e496791fb5c7ee
Aiden McCallum <aidenmax@comcast.net>
2025-03-30 13:39:00 -0500

    Made my first edition of the battleship assignment:  - Made it so my version goes 2 at a time, checking the ones underneath and besides it if it's a near miss

--- battleship/src/user_solver.cpp
+++ battleship/src/user_solver.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include "../include/user_solver.h"
 
-void UserSolver::solve(Board &board) {
-    // Your Implementation goes here
-    ReturnType t = guess(1, 1, board);
-    std::cout << t << std::endl;
+void UserSolver::solve(Board &board) 
+{
+    //Getting tracking variables
+    size_t columns = board.getX();
+    size_t rows = board.getY();
+
+    //Iterating through loop going down the columns
+    for (int i = 0; i < columns; i++) 
+    {
+        for (int j = 0; j < rows; j = j + 2) 
+        {
+            ReturnType guessedSpace = guess(i, j, board);
+
+            if (guessedSpace == ReturnType::near_miss || guessedSpace == ReturnType::hit) 
+            {
+                //Checking adjacent squares that are right and down as
+                //we already checked the above and left spaces
+                if (i < columns - 1) 
+                {
+                    guess(i + 1, j, board);
+                }
+                if (j < rows - 1) 
+                {
+                    guess(i, j + 1, board);   
+                }
+            }
+        }
+    }
 }
